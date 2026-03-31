9131f9cdd678908433e653382a1c29813c06fff5
NicoleM9 <nicolemartinez9999@gmail.com>
2026-03-31 18:48:45 -0300

    Implemento suma.py con manejo de errores

--- bloque_0/argparse/suma.py
+++ bloque_0/argparse/suma.py
@@ -1,1 +1,12 @@
+import sys
 
+suma = 0
+
+for arg in sys.argv[1:]:
+    try:
+        numero = float(arg)
+        suma += numero
+    except ValueError:
+        print(f"'{arg}' no es un número, se ignora")
+
+print(f"Suma: {suma}")
