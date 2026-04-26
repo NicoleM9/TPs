59ab5be776b2872bdbb53696b6f3e29398ec7576
NicoleM9 <nicolemartinez9999@gmail.com>
2026-04-26 14:52:48 -0300

    Ejercicio 5: sistema con web, worker y redis

--- bloque_2/proyecto/web/server.py
+++ bloque_2/proyecto/web/server.py
@@ -1,1 +1,26 @@
 
+from flask import Flask
+import redis
+import os
+import time
+
+app = Flask(__name__)
+redis_host = os.getenv("REDIS_HOST", "redis")
+
+# esperar a Redis
+while True:
+    try:
+        r = redis.Redis(host=redis_host, port=6379)
+        r.ping()
+        print("✅ Web conectado a Redis")
+        break
+    except:
+        print("⏳ Web esperando Redis...")
+        time.sleep(1)
+
+@app.route("/")
+def home():
+    valor = r.get("contador")
+    return f"Contador actual: {valor.decode() if valor else 0}"
+
+app.run(host="0.0.0.0", port=5000)
--- bloque_2/proyecto/worker/worker.py
+++ bloque_2/proyecto/worker/worker.py
@@ -1,1 +1,24 @@
+#Se implementó un sistema con tres servicios orquestados con Docker Compose. Un worker incrementa un contador en Redis periódicamente, mientras que un 
+#servidor web consulta ese valor y lo expone mediante un endpoint HTTP. Redis actúa como almacenamiento compartido entre ambos servicios.
+import redis
+import time
+import os
 
+redis_host = os.getenv("REDIS_HOST", "redis")
+
+# esperar a Redis
+while True:
+    try:
+        r = redis.Redis(host=redis_host, port=6379)
+        r.ping()
+        print("✅ Worker conectado a Redis")
+        break
+    except:
+        print("⏳ Worker esperando Redis...")
+        time.sleep(1)
+
+# loop infinito
+while True:
+    valor = r.incr("contador")
+    print(f"Worker incrementó: {valor}")
+    time.sleep(1)
